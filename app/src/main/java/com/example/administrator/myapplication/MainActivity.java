27427b31d96af2cf589e018559ed6025f4d69d0d
DaiMaZhiZaoQi <940572324@qq.com>
2019-01-24 23:49:09 +0800

    再次提交

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("print","创建了新文件");
         setContentView(R.layout.activity_main);
     }
 }
